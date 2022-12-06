daa359c6012feffabb17dcb57b2466d08d7e6386
Ragan Hagey <raganahagey@gmail.com>
2022-12-06 11:46:58 -0500

    validations

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,10 @@ class Plant < ApplicationRecord
     has_many :users, through: :user_plants 
     has_many :comments
     has_many :users, through: :comments
+
+    validates :plant_name, presence: true, uniqueness: true
+    validates :water_requirement, presence: true
+    validates :light_requirement, presence: true
+    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
     has_many :user_plants
     has_many :plants, through: :user_plants
-    has_many :comments
+    has_many :comments, dependent: :destroy
     has_many :plants, through: :comments
 
     has_secure_password
     
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
 end
